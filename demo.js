888b8b8e575404fb1d360d4c2b14920e1b1d5484
yhongm <yhongml@163.com>
2019-11-16 21:10:11 +0800

    Update demo.js

--- demo.js
+++ demo.js
@@ -1,26 +1,29 @@
-import RV from 'yhongm_rv.js'
+import RV from './src/rv.js'
 
+// import RV from './src/rv/main'
 let rv
-window.clickDiv= function () {
+
+
+window.clickDiv = function () {
     rv.data.parent = `click Div time:${new Date() / 1000}` //data变化，视图自动更新
 }
 
-window.clickP1= function () {
+window.clickP1 = function () {
     rv.data.child = `click p1 time:${new Date() / 1000}` //data变化,视图自动更新
 }
 
-window.clickP2= function () {
+window.clickP2 = function () {
     rv.data.child2 = `click p2 time:${new Date() / 1000}` //data变化,视图自动更新
 }
-window.onload = function () {
-    
-     let myData = {
+let myData = {
     parent: "parent",
     child: "child",
     pcolor: "red",
     c1color: "blue",
     c2color: "green",
     child2: "child2",
+    time: 10000,
+    componentColor: "red",
     week: [
         {
             id: 11,
@@ -35,14 +38,55 @@ window.onload = function () {
             content: "333"
         },
     ]
-   }
-    window.data = myData //控制台修改data数据，视图自动刷新内容
-       rv = new RV( //创建对象
+}
+window.data = myData
+window.RV = RV
+window.onload = function () {
+
+    var con = RV.component({
+        name: "MyComponent",
+        template: `
+            <div class="aaa" key="aaa"><p key="bbb" style="color:%#pcolor#%">"%#pcontent#%"</p><div>
+        `,
+        props: {
+            time: "1000",
+            pcontent: "a custom component"
+        },
+        data: {
+            pcontent: "a custom component",
+            pcolor: "yellow"
+
+        },
+        run() {
+
+            let colors = ['red', 'green', 'blue', 'yellow', 'gray', 'white', 'black']
+
+            setInterval(() => {
+                this.data.pcontent = this.props.pcontent
+                this.data.pcolor = colors[getRandomInt(6)]
+
+
+            }, this.props.time)
+            function getRandomInt(max) {
+                return Math.floor(Math.random() * Math.floor(max));
+            }
+
+        },
+        watch: {
+            pcolor() {
+                console.log(`pcolorChange,change:`)
+
+            }
+        }
+
+
+    })
+    rv = new RV( //创建对象
         {
             el: "#app",
             //el对象挂载的节点s
             data: myData,
-             template: `<div key="1" style="color:%#pcolor#%,width:100px,height:100px" onclick="clickDiv()">
+            template: `<div key="1" style="color:%#pcolor#%,width:100px,height:100px" onclick="clickDiv()">
                          "%#parent#%"
                          <p key="2" style="color:%#c1color#%,width:50px,height:50px" onclick="clickP1()">
                              "%#child#%"
@@ -53,25 +97,11 @@ window.onload = function () {
                          <div key="4">
                             <p key="{%#v.id#%+'_content'}" childDomData="v" for="v _in_ week"  domData="week">"%#v.content#%"</p>
                          </div>
+                         <MyComponent pcontent="ssss" color="%#componentColor#%" time="2000" key="888"></MyComponent>
                        </div>`
         })
-
-        //rv.watch("key",callback) 观察data数据对象对应key的数值变化,变化触发callback
-        rv.watch("parent", () => { alert("parent,change") })
-        
-        rv.watch("child", () => { alert("child,change") })
-        rv.watch("child2", () => { alert("child2,change") })
-    
-        function clickDiv() {
-            rv.data.parent = `click Div time:${new Date() / 1000}`//点击更新data，视图自动更新
-        }
-        function clickP1() {
-            rv.data.child = `click p1 time:${new Date() / 1000}` //点击更新data变化,视图自动更新
-        }
-        function clickP2() {
-            rv.data.child2 = `click p2 time:${new Date() / 1000}`//点击更新data变化,视图自动更新
-        }
-
+    rv.use(con)
+    rv.run()
     rv.watch("parent", () => {
         alert("parent,change")
     }) //rv.watch("key",callback) 观察data数据对象对应key的数值变化,变化触发callback
@@ -81,8 +111,11 @@ window.onload = function () {
     rv.watch("child2", () => {
         alert("child2,change")
     })
+    window.rv = rv
 
-    
+
+
+
 
 
 }
